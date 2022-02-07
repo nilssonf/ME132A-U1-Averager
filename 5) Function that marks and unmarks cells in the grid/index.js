2e7d48b28de13d5ed58b81271e6c1d2e301f0f56
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-02-07 11:19:12 +0100

    Putting it together

--- 5) Function that marks and unmarks cells in the grid/index.js
+++ 5) Function that marks and unmarks cells in the grid/index.js
@@ -52,25 +52,48 @@ the classList.
 */
 
 function createNumberDiv(){
+
     let extraDiv = document.createElement( "div" );
     extraDiv.innerHTML = randomNumber( 100 );
 
     extraDiv.addEventListener("click", function(){
         extraDiv.classList.toggle("selected");
-    })
 
-    updateResults("selected");
+          
+    });
 
 
     return extraDiv;
 }
 
+
+function gridMaker( gridContainer, R, C){
+
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+
+    for (let c = 0; c < C; c++) {
+        for (let r = 0; r < R; r++) {
+        gridContainer.appendChild( createNumberDiv() );
+        }
+    }
+}
+
+document.querySelector("button").addEventListener("click", function () {
+
+    gridMaker(
+        document.querySelector("#grid"),
+        document.querySelector("#inputRows").value,
+        document.querySelector("#inputCols").value
+    );
+});
+
+
 function randomNumber (max) {
     return Math.floor(max * Math.random());
-  }
+}
+  
 
-
-createNumberDiv();
   
 
 
